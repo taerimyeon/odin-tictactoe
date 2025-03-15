9920ad18155f10ce28f51a1736466576a5f5c78c
Steven Jonathan <48115797+taerimyeon@users.noreply.github.com>
2025-03-16 00:48:09 +0800

    Added Game State Check and Gameboard Clear
    -The winning condition will be checked on each turn, based on the current row and column. But for the diagonal, it will be checked twice (top-left to bottom-right and top-right to bottom-left diagonals) regardless of current row and column value.
    -The gameboard clear function is exposed in case a button is implemented on the frontend.
    -And again, the game can only be played through console, no interface has been developed yet.

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,9 +1,11 @@
 const gameBoard = (function Gameboard() {
-  const gameBoardData = [
+  let gameTurnCount = 0;
+  let gameBoardData = [
     ["", "", ""],
     ["", "", ""],
     ["", "", ""]
   ];
+  let isGameboardFull = false;
   const getBoardState = () => { return gameBoardData };
   const setBoardState = (player, row, column) => {
     try {
@@ -13,14 +15,60 @@ const gameBoard = (function Gameboard() {
         gameBoardData[row][column] === ""
       ) {
         gameBoardData[row][column] = player.marker;
+        const isPlayerWin = checkGameStatus(player, row, column);
+        if (isPlayerWin) {
+          return {
+            result: `${player.name} win!`
+          }
+        } 
+        if (!isPlayerWin && isGameboardFull) {
+          return {
+            result: "It's a tie!"
+          }
+        }
       }
     } catch (error) {
       console.error("[#setBoardState] error: ", error);
     }
   }
+  const checkGameStatus = (player, row, column) => {
+    try {
+      gameTurnCount++;
+      isGameboardFull = gameTurnCount === 9;
+      if (checkRow(player, row)) return true;
+      if (checkColumn(player, column)) return true;
+      if (checkDiagonal(player)) return true;
+    } catch (error) {
+      console.error("[#checkGameStatus] error: ", error);
+    }
+  }
+  const checkRow = (player, row) => {
+    const currentRowData = gameBoardData.slice(row, row+1).flat();
+    return currentRowData.every(element => element === player.marker);
+  }
+  const checkColumn = (player, column) => {
+    const currentColumnData = gameBoardData.map(data => data[column]);
+    return currentColumnData.every(element => element === player.marker);
+  }
+  const checkDiagonal = (player) => {
+    const firstDiagonal = [gameBoardData[0][0], gameBoardData[1][1], gameBoardData[2][2]];
+    const secondDiagonal = [gameBoardData[2][0], gameBoardData[1][1], gameBoardData[0][2]];
+    if (firstDiagonal.every(element => element === player.marker)) return true;
+    if (secondDiagonal.every(element => element === player.marker)) return true;
+  }
+  const clearGameboard = () => {
+    gameBoardData = [
+      ["", "", ""],
+      ["", "", ""],
+      ["", "", ""]
+    ];
+    gameTurnCount = 0;
+    isGameboardFull = false;
+  }
   return {
     getBoardState,
-    setBoardState
+    setBoardState,
+    clearGameboard
   };
 })()
 
@@ -35,9 +83,8 @@ function createPlayer(playerName, playerMarker) {
       error: "Can't use empty name!"
     }
   }
-  const player = {
+  return {
     name: playerName,
     marker: playerMarker
   }
-  return player
 }
