adf38c09eec11f312cc107e97a185299b08b77b8
Steven Jonathan <48115797+taerimyeon@users.noreply.github.com>
2025-03-22 22:59:58 +0800

    Added Gameboard Grid Display and Click Handler
    -Added gameboard grid display and click handler.
    -The marker will change depending on player turn, the first marker will be "X", followed by "O" on the next player turn.
    -The grid can't be clicked after a player wins the game or if it is a tie (through removeEventListener). If a grid is already marked, its textContent won't be modified, and the player turn won't change until the player successfully placed their marker on the empty grid.
    -The player object is currently hardcoded in the script. I have not added input forms to define the player name in this commit.
    -Updated the setBoardState() function Object return. If the game is finished the result string is "finished" accompanied with the following message: a tie, or ${playerName} wins the game. If a grid is marked for the first time, the result string is "ongoing" accompanied with a message ${playerName} marked the grid. If a grid is already marked, or if there is an error, the result string is "error" accompanied with the error message.

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -6,7 +6,9 @@ const gameBoard = (function Gameboard() {
     ["", "", ""]
   ];
   let isGameboardFull = false;
-  const getBoardState = () => { return gameBoardData };
+  const getBoardState = (row, column) => {
+    return gameBoardData[row][column]
+  };
   const setBoardState = (player, row, column) => {
     try {
       if (
@@ -18,17 +20,33 @@ const gameBoard = (function Gameboard() {
         const isPlayerWin = checkGameStatus(player, row, column);
         if (isPlayerWin) {
           return {
-            result: `${player.name} win!`
+            result: "finished",
+            message: `${player.name} win!`
           }
         } 
         if (!isPlayerWin && isGameboardFull) {
           return {
-            result: "It's a tie!"
+            result: "finished",
+            message: "It's a tie!"
           }
+        }
+        return {
+          result: "ongoing",
+          message: `${player.name} placed ${player.marker} at Row: ${row+1} Col: ${column+1}`
+        }
+      } else {
+        const mark = getBoardState(row, column);
+        return {
+          result: "error",
+          message: `The grid at Row: ${row+1} Col: ${column+1} is already marked with ${mark}`
         }
       }
     } catch (error) {
       console.error("[#setBoardState] error: ", error);
+      return {
+        result: "error",
+        message: error
+      }
     }
   }
   const checkGameStatus = (player, row, column) => {
@@ -56,7 +74,7 @@ const gameBoard = (function Gameboard() {
     if (firstDiagonal.every(element => element === player.marker)) return true;
     if (secondDiagonal.every(element => element === player.marker)) return true;
   }
-  const clearGameboard = () => {
+  const resetGameboard = () => {
     gameBoardData = [
       ["", "", ""],
       ["", "", ""],
@@ -65,12 +83,74 @@ const gameBoard = (function Gameboard() {
     gameTurnCount = 0;
     isGameboardFull = false;
   }
+  // Expose the function(s) outside
   return {
     getBoardState,
     setBoardState,
-    clearGameboard
+    resetGameboard
   };
 })()
+
+const displayController = (function DisplayController() {
+  const gridMapping = {
+    one: [0, 0],
+    two: [0, 1],
+    three: [0, 2],
+    four: [1, 0],
+    five: [1, 1],
+    six: [1, 2],
+    seven: [2, 0],
+    eight: [2, 1],
+    nine: [2, 2]
+  }
+  const setGridMarker = (player, eventObject) => {
+    try {
+      const gridId = eventObject.target.id.split("-").pop();
+      const row = gridMapping[gridId][0];
+      const column = gridMapping[gridId][1];
+      const result = gameBoard.setBoardState(player, row, column);
+      if (result.result !== "error") eventObject.target.textContent = player.marker;
+      return result
+    } catch (error) {
+      console.error("[#setGridMarker] error: ", error);
+    }
+  }
+  // Expose the function(s) outside
+  return {
+    setGridMarker
+  }
+})()
+
+function playGame(event) {
+  let result
+  if (turn === "playerOne") {
+    result = displayController.setGridMarker(playerOne, event);
+    console.log(result);
+  } else {
+    result = displayController.setGridMarker(playerTwo, event);
+    console.log(result);
+  }
+  if (result.result === "finished") removeGridEventListener();
+  if (result.result === "ongoing") turn = turn === "playerOne" ? "playerTwo" : "playerOne";
+}
+
+function addGridEventListener() {
+  const gridElements = document.getElementsByClassName("gameboard-grid-item");
+  if (gridElements) {
+    for (const gridElement of gridElements) {
+      gridElement.addEventListener("click", playGame);
+    }
+  }
+}
+
+function removeGridEventListener() {
+  const gridElements = document.getElementsByClassName("gameboard-grid-item");
+  if (gridElements) {
+    for (const gridElement of gridElements) {
+      gridElement.removeEventListener("click", playGame);
+    }
+  }
+}
 
 function createPlayer(playerName, playerMarker) {
   if (playerMarker !== "O" && playerMarker !== "X") {
@@ -88,3 +168,10 @@ function createPlayer(playerName, playerMarker) {
     marker: playerMarker
   }
 }
+
+const playerOne = createPlayer("Player_1", "X")
+const playerTwo = createPlayer("Player_2", "O")
+let turn = "playerOne"
+
+// On DOM mounted
+document.addEventListener("DOMContentLoaded", addGridEventListener)
